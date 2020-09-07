3ece4a3e85c5b77d0e9522509673aaf1d7699076
Hifsah Khan <s344235@oslomet.no>
2020-09-07 13:21:28 +0200

    Merge branch 'master' of /Users/hifsahkhan/IdeaProjects/Oblig1.git with conflicts.

--- no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -4,13 +4,19 @@ package no.oslomet.cs.algdat.Oblig1;
 
 import java.lang.UnsupportedOperationException;
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 
 public class Oblig1 {
     private Oblig1() {}
 
     ///// Oppgave 1 //////////////////////////////////////
     public static int maks(int[] a) {
+
         throw new UnsupportedOperationException();
+        for(int i = 1; i < a.length; i++ ){
+
+
+        }
     }
 
     public static int ombyttinger(int[] a) {
@@ -24,13 +30,58 @@ public class Oblig1 {
 
     ///// Oppgave 3 //////////////////////////////////////
     public static int antallUlikeUsortert(int[] a) {
-        throw new UnsupportedOperationException();
+        int n=a.length;
+
+        if(n<2){
+            throw new UnsupportedOperationException("Intervallet er ugyldig");
+        }
+
+        int teller=0;
+        for(int i=0; i<n; ++i){
+            boolean uniktTall=false;
+
+            for(int j=0; j<i; j++){
+                if(a[i]==a[j]){
+                    uniktTall=true;
+                    break;
+                }
+            }
+            if(!uniktTall){
+                teller++;
+            }
+        }
+        System.out.println("Det er "+teller+" forskjellige verdier i tabellen");
+        return teller;
     }
 
     ///// Oppgave 4 //////////////////////////////////////
     public static void delsortering(int[] a) {
-        throw new UnsupportedOperationException();
+        int n = a.length;
+
+        if(n<2){
+            throw new UnsupportedOperationException("Intervallet er ugyldig");
+        }
+
+        int ptall = n - 1;
+        int otall = 0;
+
+        while (true) {
+            while (ptall >= 0 && a[ptall] % 2 == 0) {
+                ptall--;
+            }
+            while (otall < a.length && a[otall] % 2 != 0) {
+                otall++;
+            }
+            if (otall < ptall) {
+               Oblig1Test.bytt(a, otall, ptall);
+            } else {
+                break;
+            }
+        }
+        Arrays.sort(a, 0, otall);
+        Arrays.sort(a, otall, n);
     }
+
     //Oppgave 5
     public static void rotasjon(char[] a) {
         int n = a.length;
@@ -40,8 +91,6 @@ public class Oblig1 {
                 a[i] = a[i - 1];
             }
             a[0] = temp;
-
-
     }
 
     //Oppgave 6
@@ -58,85 +107,57 @@ public class Oblig1 {
             a[i] = a[i - k];
         }
         System.arraycopy(d,0, a, 0, k);
-
-
     }
 
     ///// Oppgave 7 //////////////////////////////////////
     /// 7a)
-    public static String flett(String s, String t){
-        StringBuilder sb = new StringBuilder();
-        int i = 0, j = 0, k = 0;                 // lokkevariabler
-
-        while (i < s.length() - 1 && j < t.length()) {
-            sb.append(s.charAt(i++));      // forst en verdi fra s
-            sb.append(t.charAt(j++));       // saa en verdi fra t
-        }
-        // vi maa ta med resten
-
-        if (i < s.length()) sb.append(s.substring(i));
-        if (j < t.length()) sb.append(t.substring(j));
-
-        return sb.toString();
+    public static String flett(String s, String t) {
+        throw new UnsupportedOperationException();
     }
 
     /// 7b)
-    public static String flett(String... s){
-        StringBuilder sb2 = new StringBuilder();
-
-        boolean done = false;
-        int i = 0;
-        while (!done) {
-            done = true;
-
-            for (int j = 0; j < s.length; j++) {
-                if (s[j].length() > i) {
-                    sb2.append(s[j].charAt(i));
-
-                    done = false;
-                }
-            }
-            i++;
-
-        }
-        return sb2.toString();
+    public static String flett(String... s) {
+        throw new UnsupportedOperationException();
     }
 
     ///// Oppgave 8 //////////////////////////////////////
     public static int[] indekssortering(int[] a) {
-
-        int[] index = new int[a.length];
-        int t;
-        int x;
-
-        for (int i = 0; i < a.length; i++) {
-            index[i] = i;
-        }
-
-
-        for (int i = 1; i < a.length; i++) {
-            for (t = index[i], x = i - 1; x >= 0 && a[t] < a[index[x]]; x--) {
-
-                index[x + 1] = index[x];
-            }
-            index[x + 1] = t;
-        }
-        return index;
+        throw new UnsupportedOperationException();
     }
-
 
     ///// Oppgave 9 //////////////////////////////////////
     public static int[] tredjeMin(int[] a) {
         throw new UnsupportedOperationException();
     }
 
-    ///// Oppgave 10 //////////////////////////////////////
+    ///// Oppgave 10 ////////////////////////////////////// ?????
     public static int bokstavNr(char bokstav) {
         throw new UnsupportedOperationException();
+
     }
 
     public static boolean inneholdt(String a, String b) {
-        throw new UnsupportedOperationException();
+        if(a.equals(" ")|| b.equals(" ")||a.equals("")||b.equals("")){
+            throw new UnsupportedOperationException("Ikke gyldig String-input");
+        }
+        int[] a1 = new int[256];
+        int[] b1 = new int[256];
+        int n = a1.length;
+
+        for (char c : a.toCharArray()){
+            a1[c]++;
+        }
+        for (char c : b.toCharArray()){
+            b1[c]++;
+        }
+        for (int i=0; i<n; i++){
+            if (a1[i]>0){
+                if (a1[i]<b1[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }  // Oblig1
